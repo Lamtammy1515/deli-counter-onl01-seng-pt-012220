9fea194bfed35ec78dce4f7169a0a146cb3bb4bd
Tammy Lam <lamtammy1515@gmail.com>
2020-01-27 23:39:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 require 'pry'
 def line(kats_deli)
-array= []
-status = "The line is currently: "
+wait_list = []
+
 if kats_deli.length == 0 
   puts "The line is currently empty."
 elsif kats_deli.each.with_index do |name, index| 
- array << "#{index +1}." "#{name}"
+ wait_list << "#{index +1}." "#{name}"
  end
  end 
  return "#{status} #{array.join}"
