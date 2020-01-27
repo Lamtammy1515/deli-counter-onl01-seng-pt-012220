3fe1a16254d08277cc023ad66dd518cde522d62f
Tammy Lam <lamtammy1515@gmail.com>
2020-01-27 23:06:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+# require 'pry'
+def line(kats_deli)
+  
+status = "The line is currently: "
+kats_deli.each.with_index do |name, index| 
+ status << "#{index +1}." "#{name}"
+ end 
+ return status
+end
+
+
+
+def take_a_number(kats_deli, name)
+  phrase = 
+  
+end 
