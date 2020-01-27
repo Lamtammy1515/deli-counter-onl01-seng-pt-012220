f56b587114aa7f6ebdc2de97be8f1b137f4d1913
Tammy Lam <lamtammy1515@gmail.com>
2020-01-27 23:25:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,8 @@
 def line(kats_deli)
 array= []
 status = "The line is currently: "
+if kats_deli.length == 0 
+  puts "T"
 kats_deli.each.with_index do |name, index| 
  array << "#{index +1}." "#{name}"
  end 
