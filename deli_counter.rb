3b3dbcdb6cce8304d1b0f562e2c604e95fde1b4f
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 00:32:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def take_a_number(kats_deli, name)
 end 
 
 def now_serving(kats_deli)
-  if kats_deli == 0 
+  if kats_deli.empty? 
   puts "There is nobody waiting to be served!"
 else 
   puts "Currently serving #{kats_deli.first}."
